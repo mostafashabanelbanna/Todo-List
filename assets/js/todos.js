abd34ce6cb69731d091406875759befdbb80a68b
Mostafa <dashassas@gmail.com>
2019-02-21 02:48:19 +0200

    click on X span to delete Todo

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -2,3 +2,10 @@
 $('li').on('click', function() {
 	$(this).toggleClass('completed');
 });
+//click on X to delete Todo
+$('span').on('click', function(e) {
+	$(this).parent().fadeOut(500, function() {
+		$(this).remove();
+	});
+	e.stopPropagation();
+});
